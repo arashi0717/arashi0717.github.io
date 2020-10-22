d6d32bcda42bfdd166c480e372b44c795c257d7d
arashi0717 <38757495+arashi0717@users.noreply.github.com>
2020-10-23 01:07:44 +0800

    主页面布局调整

--- birthday/js/index.js
+++ birthday/js/index.js
@@ -1,11 +1,14 @@
 function changeFrameSize(){
+	//设置iframe的宽高
 	var ifm = document.getElementById("iframePage");
 	ifm.height = document.documentElement.clientHeight;
-	ifm.width = $(".container").outerWidth()
+	ifm.width = document.documentElement.clientWidth;
+	//去掉空白处
+	$(".container-fluid").css({"margin":"0","padding":"0"});
 }
 
 window.onresize = function() {
 	changeFrameSize();
 }
 
- document.getElementById("iframePage").onload = changeFrameSize;
+ document.getElementById("iframePage").onload = changeFrameSize();
--- birthday/js/main.js
+++ birthday/js/main.js
@@ -34,5 +34,34 @@ function requestFullScreen(element) {
 }
 
 $(document).ready(function(){
-	$("#lable1").fadeIn(5000);
+	if($("body").outerWidth()<=576){
+		$(".container li").css("font-size","32px");
+	}else{
+		$(".container li").css("font-size","40px");
+	}
+	$("#title1").fadeIn(5000)
+	setTimeout(function(){
+		if($("body").outerWidth()<=576){
+			$("#title2").show().animate({fontSize:'20px'},3000);
+		}else{
+			$("#title2").show().animate({fontSize:'28px'},3000);
+		}
+		setTimeout(function(){
+			$("#fairy1").removeAttr("hidden");
+			$("#fairy2").removeAttr("hidden");
+			$("#fairy3").removeAttr("hidden");
+			$("#fairy4").removeAttr("hidden");
+		},3500)
+	},5500);
+	
+});
+
+$(window).resize(function(){
+	if($("body").outerWidth()<=576){
+		$(".container li").css("font-size","32px");
+		$(".container span").css("font-size","20px");
+	}else{
+		$(".container li").css("font-size","40px");
+		$(".container span").css("font-size","28px");
+	}
 });
