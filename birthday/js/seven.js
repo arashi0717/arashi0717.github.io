e7f5cc6d3140c32bb2773bc792009bcb8f925a81
arashi0717 <38757495+arashi0717@users.noreply.github.com>
2020-10-24 22:08:40 +0800

    图片加载缓慢解决方案2

--- birthday/js/seven.js
+++ birthday/js/seven.js
@@ -14,3 +14,25 @@ $(document).bind("mobileinit", function()
 $('.suprise').click(function () {
 	location.href = "BirthdayCake.html";
 });
+
+//Loading页面
+document.onreadystatechange = function () {
+	if (document.readyState == "complete") {
+		loadingFade();
+	}
+}
+
+function loadingFade() {
+	var opacity = 1;
+	//var loadingPage=document.getElementById('loading');
+	var loadingBackground = document.getElementById('loading_bg');
+	var time = setInterval(function () {
+		if (opacity <= 0) {
+			clearInterval(time);
+			$('#loading').remove();
+		}
+
+		loadingBackground.style.opacity = opacity;
+		opacity -= 0.4;
+	}, 100);
+}
