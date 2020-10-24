b00f26d4ff54bfc10657f4e21c317943805e769e
arashi0717 <38757495+arashi0717@users.noreply.github.com>
2020-10-24 22:50:32 +0800

    简化图片加载缓慢解决方案2

--- birthday/js/seven.js
+++ birthday/js/seven.js
@@ -1,42 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$(".suprise").delay(1500).fadeIn(3500);
 });
 
-$(document).bind("mobileinit", function()   
-{   
-   if (navigator.userAgent.indexOf("Android") != -1)   
-   {   
-     $.mobile.defaultPageTransition = 'none';   
-     $.mobile.defaultDialogTransition = 'none';   
-   }   
+$(document).bind("mobileinit", function () {
+	if (navigator.userAgent.indexOf("Android") != -1) {
+		$.mobile.defaultPageTransition = 'none';
+		$.mobile.defaultDialogTransition = 'none';
+	}
 });
 
 $('.suprise').click(function () {
 	location.href = "BirthdayCake.html";
 });
 
-//Loading页面
-// document.onreadystatechange = function () {
-// 	if (document.readyState == "complete") {
-// 		loadingFade();
-// 	}
-// }
-sevenImg.onload = function(){
-	loadingFade();
-}
-
-function loadingFade() {
-	var opacity = 1;
-	//var loadingPage=document.getElementById('loading');
-	var loadingBackground = document.getElementById('loading_bg');
-	var time = setInterval(function () {
-		if (opacity <= 0) {
-			clearInterval(time);
-			$('#loading').remove();
+function imgLoad(img, callback) {
+	var timer = setInterval(function () {
+		if (img.complete) {
+			callback(img)
+			clearInterval(timer)
 		}
-
-		loadingBackground.style.opacity = opacity;
-		opacity -= 0.4;
-	}, 100);
+	}, 50)
 }
+imgLoad(sevenImg, function () {
+	
+	$('.loading').remove();
+});
 
